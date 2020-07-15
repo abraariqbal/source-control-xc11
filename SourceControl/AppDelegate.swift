e2205fae10d620e47f8d6d996b8d063a6b7c0433
Abraar Iqbal <abraar.iqbal@gmail.com>
2020-07-14 19:02:46 -0500

    Initial Commit

---
+++ SourceControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SourceControl
+//
+//  Created by Abraar Iqbal on 7/14/20.
+//  Copyright © 2020 Abraar Iqbal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SourceControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SourceControl
+//
+//  Created by Abraar Iqbal on 7/14/20.
+//  Copyright © 2020 Abraar Iqbal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
